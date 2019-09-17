85a32a3194327350d1247da7838788fae3f3c16e
Paramee Thongtep <zai36997@gmail.com>
2019-09-17 13:59:42 +0700

    connect mongodb and setting environment.

---
+++ nodejs/server.js
@@ -0,0 +1,34 @@
+
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  cors = require("cors"),
+  app = express(),
+  mongoose = require("mongoose"),
+  config = require('./config/index')
+
+  port = process.env.PORT
+  // router = express.Router();
+  
+  
+  
+  app.use(bodyParser.json());
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+// replace the uri string with your connection string.
+
+mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true , useCreateIndex: true,useUnifiedTopology: true}).then(
+    // เมื่อการเชื่อมต่อสำเร็จ
+    () => {
+      console.log("Database connection is successful");
+    },
+    // เมื่อการเชื่อมต่อไม่สำเร็จ
+    err => {
+      console.log("Error when connecting to the database" + err);
+    }
+  );
+  app.listen(port, function() {
+    console.log("Listening on port " + port);
+  });
